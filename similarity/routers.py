992fc1d712973d7799603d8358fda44fc9f80b66
hengyinayo <wuheng@yonsei.ac.kr>
2024-02-17 23:39:45 +0900

    feat: foreign similarity update

--- similarity/routers.py
+++ similarity/routers.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, APIRouter, HTTPException
 from pydantic import BaseModel
 from typing import List
+import urllib.request
+from similarity.translation import korean_to_english
 from similarity.gpt_func import IPC_filtering
 from similarity.database import foreign_top_similarity, korea_top_similarity
 
@@ -13,14 +15,14 @@ class SimilarityQuery(BaseModel):
 router = APIRouter(prefix="/similarity-check")
 
 @router.post("")
-async def search_top_similarity(query_data: SimilarityQuery):
+def search_top_similarity(query_data: SimilarityQuery):
     try:
-        query = query_data.query
+        korean_query = query_data.query
         n_results = query_data.n_results
-        IPC_codes = IPC_filtering(query)
-        korean_results = korea_top_similarity(query, n_results, IPC_codes)
-        foreign_results = foreign_top_similarity(query, n_results, IPC_codes)
-
+        IPC_codes = IPC_filtering(korean_query)
+        english_query = korean_to_english(korean_query)
+        korean_results = korea_top_similarity(korean_query, n_results, IPC_codes)
+        foreign_results = foreign_top_similarity(english_query, n_results, IPC_codes)
 
         return {"korean_results": korean_results, "foreign_results": foreign_results}
     except Exception as e:
